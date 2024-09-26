5653aca88673299ec7e3c3f07da057daf7703381
ayan3018 <ayan.pakhira3018@gmail.com>
2024-09-26 11:43:07 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,93 @@
+import { useState } from 'react'
+import { InputBox } from './components';
+import useCurrencyInfo from './hooks/useCurrencyInfo'
+
+function App() {
+
+    const [amount, setAmount] = useState(0)
+    const [from, setFrom] = useState("usd")
+    const [to, setTo] = useState("inr")
+    const [convertedAmount, setConvertedAmount] = useState(0)
+
+
+    const currencyInfo = useCurrencyInfo(from)
+    //as we know that this hooks takes the valueof currency as a parameter
+    //so here we need to give the value of from as the parameter
+    //as we need to convert the amount of 'from' to 'to'
+
+    const options = Object.keys(currencyInfo)
+
+    //for swapping the currency between from and to
+    const swap = () => {
+        setFrom(to)
+        setTo(from)
+        setConvertedAmount(amount)
+        setAmount(convertedAmount)
+    }
+
+    //for displaying the final amount
+    const finalResult = () => {setConvertedAmount(amount * currencyInfo[to])}  
+
+    return (
+        <div
+            className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
+            style={{
+                backgroundImage: `url('https://images.pexels.com/photos/6927390/pexels-photo-6927390.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')`,
+            }}
+        >
+            <div className="w-full">
+                <div className="w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
+                    <form
+                        onSubmit={(e) => {
+                            e.preventDefault();
+                            finalResult()
+                           
+                        }}
+                    >
+                        <div className="w-full mb-1">
+                            <InputBox
+                                label="From"
+                                amount={amount}
+                                currencyOptions={options}
+                                onCurrencyChange={(currency) => setAmount(amount)}
+                                selectCurrency={from}
+                                onAmountChange={(amount) => setAmount(amount)}
+                                
+                            />
+                        </div>
+                        <div className="relative w-full h-0.5">
+                            <button
+                                type="button"
+                                className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5"
+                                onClick={swap}
+                            >
+                                swap
+                            </button>
+                        </div>
+                        <div className="w-full mt-1 mb-4">
+                            <InputBox
+                                label="To"
+                                amount={convertedAmount}
+                                currencyOptions={options}
+                                onCurrencyChange={(currency) => setTo(currency)}
+                                //need to check this, why this field is different \
+                                //from the 'from' field.
+                                selectCurrency={from}
+                                amountDisable
+
+                                
+                            />
+                        </div>
+                        <button type="submit" className="w-full bg-blue-600 text-white px-4 py-3 rounded-lg">
+                            Convert {from.toUpperCase()} to {to.toUpperCase()}
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default App
+
+// 
